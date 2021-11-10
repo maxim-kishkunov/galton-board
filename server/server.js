1b264ab1fb58488f118333219c0df48f50d73c99
maxim-kishkunov <maxim.kishkunov@gmail.com>
2021-11-10 22:06:19 +0200

    1

--- server/server.js
+++ server/server.js
@@ -6,12 +6,16 @@ const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 dotenv.config();
 
+const connectStr = process.env.DATABASE_URL;
+
 const db = new pg.Pool({
- host: process.env.DB_HOST,
- database: process.env.DB_NAME,
- user: process.env.DB_USER,
- password: process.env.DB_PASSWORD,
- port: process.env.DB_PORT
+  connectionString: connectStr,
+  ssl: true
+//  host: process.env.DB_HOST,
+//  database: process.env.DB_NAME,
+//  user: process.env.DB_USER,
+//  password: process.env.DB_PASSWORD,
+//  port: process.env.DB_PORT
 });
 const PORT = process.env.PORT || 3001;
 
